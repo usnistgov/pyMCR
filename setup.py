928d9dbb6e3f547d63e43e3c9b5d1c7d6738e685
Charles H. Camp Jr <ccampjr@gmail.com>
2017-12-13 03:33:52 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""
+
+"""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(name='pyMCR',
+      version = '0.1.0a',
+      description = 'Multivariate Curve Resolution in Python',
+      long_description = long_description,
+      url = 'https://github.com/CCampJr/pyMCR',
+      author = 'Charles H. Camp Jr.',
+      author_email = 'charles.camp@nist.gov',
+      license = '',
+      packages = find_packages(),
+      zip_safe = False,
+      include_package_data = True,
+      install_requires=['numpy'],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Science/Research',
+                   'Operating System :: OS Independent',
+                   'Environment :: X11 Applications :: Qt',
+                   'Programming Language :: Python :: 3 :: Only',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Topic :: Scientific/Engineering :: Bio-Informatics',
+                   'Topic :: Scientific/Engineering :: Chemistry',
+                   'Topic :: Scientific/Engineering :: Information Analysis',
+                   'Topic :: Scientific/Engineering :: Mathematics',
+                   'Topic :: Scientific/Engineering :: Physics'])
