30855b883a43641d27ad6845dfff691eba6fec35
Charles H. Camp Jr <ccampjr@gmail.com>
2017-12-18 23:20:31 -0500

    Changed version to 0.1.1a0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
     long_description = f.read()
 
 setup(name='pyMCR',
-      version = '0.1.1a',
+      version = '0.1.1a0',
       description = 'Multivariate Curve Resolution in Python',
       long_description = long_description,
       url = 'https://github.com/CCampJr/pyMCR',
