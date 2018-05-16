6aa33b9602c6f7229646ca79f4e9e3d2c35349ac
Charles H. Camp Jr <charles.camp@nist.gov>
2018-05-16 01:47:52 -0400

    Bump to version 0.2.1a0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
     long_description = f.read()
 
 setup(name='pyMCR',
-      version = '0.2.0',
+      version = '0.2.1a0',
       description = 'Multivariate Curve Resolution in Python',
       long_description = long_description,
       url = 'https://github.com/CCampJr/pyMCR',
