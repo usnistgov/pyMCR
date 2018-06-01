ae8753aaccd3c885d25b788d4409f6fb4dce99b8
sshojiro <shojiro.shibayama@gmail.com>
2018-06-01 23:59:19 +0900

    Replace all sklearn methods to reduce dependency

--- pymcr/rank.py
+++ pymcr/rank.py
@@ -1,37 +1,40 @@
 
 import numpy as np
-from sklearn.decomposition import PCA
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+from numpy.linalg import svd
 
-# FIXME: avoid sklearn implementations.
-#        Just use numpy libraries: e.g. PCA => numpy.linalg.svd
-def ind(D_actual, scale=True, ul_rank=100):
+__all__ = ['ind', 'rod']
+
+
+def ind(D_actual, ul_rank=100):
     """ Malinowski's indicator function """
     n_samples = D_actual.shape[0]
     n_max_rank = np.min([ul_rank, np.min(D_actual.shape)-1])
     error_squared = np.zeros(n_max_rank)
 
-    # PCA forces data matrices to be centered.
-    # Therefore, D_actual also needs to be centered.
-    D_scale = StandardScaler(with_std=scale).fit_transform(D_actual)
-
-    if scale:
-        model = Pipeline([('scale', StandardScaler()),
-                          ('pca', PCA(n_components=n_max_rank))])
-    else:
-        model = Pipeline([('pca', PCA(n_components=n_max_rank))])
-    pca_scores = model.fit_transform(D_actual)
+    # PCA forces data matrices to be normalized.
+    # Therefore, D_actual also needs to be normalized.
+    D_scale = _scale(D_actual)
+    U, S, _ = svd(D_actual)
+    T = U * S
     for n_rank in range(1, n_max_rank+1):
-        error_squared[n_rank - 1] = np.sum(np.square(D_scale)) - np.sum(np.square(pca_scores[:, :n_rank]))
+        error_squared[n_rank - 1] = np.sum(np.square(D_scale)) - np.sum(np.square(T[:, :n_rank]))
     indicator = np.sqrt(error_squared) /\
                 np.square([n_samples - L for L in np.arange(1, n_max_rank+1)])
     return indicator
 
 
-def rod(D_actual, scale=True, ul_rank=100):
+def rod(D_actual, ul_rank=100):
     """ Ratio of derivatives """
-    IND = ind(D_actual, scale, ul_rank)
+    IND = ind(D_actual, ul_rank)
     ROD = ( IND[0:(len(IND)-2)] - IND[1:(len(IND)-1)] ) \
           / ( IND[1:(len(IND)-1)] - IND[2:len(IND)] )
     return ROD
+
+
+def _scale(X, with_mean=True, with_std=True):
+    Xsc = X
+    if with_mean:
+        Xsc -= X.mean(axis=0)
+    if with_std:
+        Xsc /= X.std(axis=0)
+    return Xsc
