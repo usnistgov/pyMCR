0dd5d1acf3651ba3a5c9a5ff1b6d4b842a7afd7c
Charles H. Camp Jr <ccampjr@gmail.com>
2018-06-01 14:07:48 -0400

    Added axis and copy parameters

--- pymcr/rank.py
+++ pymcr/rank.py
@@ -1,6 +1,6 @@
 
 import numpy as _np
-from numpy.linalg import _svd
+from numpy.linalg import svd as _svd
 
 __all__ = ['ind', 'rod']
 
@@ -31,10 +31,37 @@ def rod(D_actual, ul_rank=100):
     return ROD
 
 
-def _scale(X, with_mean=True, with_std=True):
-    Xsc = X
-    if with_mean:
-        Xsc -= X.mean(axis=0)
+def _scale(X, mean_ctr=True, with_std=True, axis=-1, copy=True):
+    """
+    Standardization of data
+    
+    Parameters
+    ----------
+
+    X : ndarray
+        Data array
+
+    mean_ctr : bool
+        Mean-center data
+
+    with_std : bool
+        Normalize by the standard deviation of the data
+    
+    axis : int
+        Axis from which to calculate mean and standard deviation
+
+    copy : bool
+        Copy data (X) if True, overwite if False
+
+    """
+
+    if copy:
+        Xsc = 1*X
+    else:
+        Xsc = X
+
+    if mean_ctr:
+        Xsc -= X.mean(axis=axis, keepdims=True)
     if with_std:
-        Xsc /= X.std(axis=0)
+        Xsc /= X.std(axis=axis, keepdims=True)
     return Xsc
