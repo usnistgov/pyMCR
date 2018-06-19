907ade70ec5f0572af72ffbd7c2c02ee42afe734
sshojiro <shojiro.shibayama@gmail.com>
2018-06-19 16:42:16 +0900

    Remoted the previous redundunt code to enhance readability
    Also added TODOs for future correction

--- pymcr/rank.py
+++ pymcr/rank.py
@@ -43,48 +43,45 @@ def pca(D, n_components=None):
     assert _np.allclose(W, Wt.T)
     T = _np.dot(D, W)
 
-    return (T, W, s2)
+    return T, W, s2
+
 
 def ind(D_actual, ul_rank=100):
-    """ Malinowski's indicator function """
+    """ TODO: update docstring
+    Malinowski's indicator function """
     n_samples = D_actual.shape[0]
     n_max_rank = _np.min([ul_rank, _np.min(D_actual.shape)-1])
-    error_squared = _np.zeros(n_max_rank)
 
     T, W, s2 = pca(D_actual)
+    D_centered = _standardize(D_actual, with_std=False)
 
-    # ! PCA only centers the mean, it does not divide by the standard deviation
-    # PCA forces data matrices to be normalized.
-    # Therefore, D_actual also needs to be normalized.
-    # D_scale = _standardize(D_actual)
-    D_scale = D_actual - D_actual.mean(axis=0, keepdims=True)
-    # U, S, _ = _svd(D_actual)
-    # T = U * S
+    # error_squared is equal to projection errors of PCA.
+    error_squared = _np.sum(D_centered**2) - \
+                    _np.sum(_np.cumsum(T[:,:-1]**2, axis=-1), axis=0)
+    # indicator is a standardized statistic
+    indicator = _np.sqrt(error_squared) / \
+                (n_samples - _np.arange(1, n_max_rank+1))**2
 
-    error_squared = _np.sum(D_scale**2) - _np.sum(_np.cumsum(T[:,:-1]**2, axis=-1), axis=0)
-
-    indicator = _np.sqrt(error_squared) / (n_samples - _np.arange(1, n_max_rank+1))**2
-
+    # TODO: replace the code above with the following one
     # n_samples = D_actual.shape[0]
     # n_features = D_actual.shape[1]
-
     # assert s2.size == n_features, 'Number of samples must be larger than number of features'
     # k_vec = _np.arange(n_features) + 1
-
     # eigen_values = _np.sqrt(s2)
-
     # indicator = _np.sqrt(_np.cumsum(eigen_values) / (n_samples * (n_features - k_vec))) / (n_samples - k_vec)**2
 
     return indicator
 
 
 def rod(D_actual, ul_rank=100):
-    """ Ratio of derivatives """
+    """ TODO: update docstring
+    Ratio of derivatives """
     IND = ind(D_actual, ul_rank)
     ROD = ( IND[0:(len(IND)-2)] - IND[1:(len(IND)-1)] ) \
           / ( IND[1:(len(IND)-1)] - IND[2:len(IND)] )
     return ROD
 
+
 if __name__ == '__main__':
     D = _np.vstack((_np.eye(3), -_np.eye(3)))
-    ind(D)
+    print(ind(D))
