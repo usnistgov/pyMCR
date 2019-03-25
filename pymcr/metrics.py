134b5accabe2dd04718237b1f4596269e28bb0b4
ClarkAH <44094404+ClarkAH@users.noreply.github.com>
2019-03-25 17:09:39 +0100

    Delete metrics.py

--- pymcr/metrics.py
+++
@@ -1,9 +0,0 @@
-"""
-Metrics used in pyMCR
-
-All functions must take C, ST, D_actual, D_calculated
-"""
-
-def mse(C, ST, D_actual, D_calculated):
-    """ Mean square error"""
-    return ((D_actual - D_calculated)**2).sum()/D_actual.size
