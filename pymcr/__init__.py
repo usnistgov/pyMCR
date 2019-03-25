84e0fc8cb9f6cd772470e278268bace2fcf41f05
ClarkAH <44094404+ClarkAH@users.noreply.github.com>
2019-03-25 18:06:24 +0100

    Add files via upload

---
+++ pymcr/__init__.py
@@ -0,0 +1,6 @@
+""" pyMCR: Pythonic Multivariate Curve Resolution - Alternating Least Squares """
+from . import mcr
+from . import constraints
+from . import metrics
+from . import regressors
+from . import simplisma
