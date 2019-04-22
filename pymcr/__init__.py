7080707cb4abe3a01fd193f096366be307936681
Charles <chc@NIST.GOV>
2019-04-22 15:42:15 -0400

    Docs: Improved pyMCR docstring

--- pymcr/__init__.py
+++ pymcr/__init__.py
@@ -1,4 +1,15 @@
-""" pyMCR: Pythonic Multivariate Curve Resolution - Alternating Least Squares """
+""" 
+pyMCR: Pythonic Multivariate Curve Resolution - Alternating Regression
+
+pyMCR is a small package for performing multivariate curve resolution. Currently, 
+it implements a simple alternating regression scheme (MCR-AR). The most common 
+implementation is with ordinary least-squares regression, MCR-ALS.
+
+See Also
+--------
+https://pages.nist.gov/pyMCR
+
+"""
 from . import mcr
 from . import constraints
 from . import metrics
