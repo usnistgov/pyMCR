3b013a89cdf2ac73218e6867d1e68d35cf467ea5
Charles H. Camp Jr <charles.camp@nist.gov>
2021-08-27 14:11:32 -0400

    Logging setup in init by default
    * User no longer needs to setup logging
    * Bug fixes and updates to code (e.g., boxplot whis=[0,100])

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -8,6 +8,13 @@ This document records all notable changes to
 This project adheres to `PEP 440 -- Version Identification 
 and Dependency Specification <https://www.python.org/dev/peps/pep-0440/>`_.
 
+0.3.3 ()
+---------
+
+- Logging setup by default upon importing the library.
+- Updated Jupyter Notebooks to reflect the change to the logging setup
+- Minor bug fixes
+
 0.3.2 (19-06-25)
 ----------------
 
--- pymcr/__init__.py
+++ pymcr/__init__.py
@@ -4,3 +4,18 @@ from . import constraints
 from . import metrics
 from . import regressors
 from . import condition
+
+import sys
+import logging as _logging
+
+_logger = _logging.getLogger('pymcr')
+_logger.setLevel(_logging.DEBUG)
+
+# StdOut is a "stream"; thus, StreamHandler
+stdout_handler = _logging.StreamHandler(stream=sys.stdout)
+
+# Set the message format. Simple and removing log level or date info
+stdout_format = _logging.Formatter('%(message)s')  # Just a basic message akin to print statements
+stdout_handler.setFormatter(stdout_format)
+
+_logger.addHandler(stdout_handler)
