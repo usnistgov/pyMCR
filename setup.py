dfb43ae5d45bf64e8d64d87513ee5170284d60e4
Charles H. Camp Jr <charles.camp@nist.gov>
2021-10-22 17:10:54 -0400

    Version and install reqires come from files

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 """
 Setup for pyMCR
 """
+import os
 import io
 from setuptools import setup, find_packages
 
 with io.open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
+with open('./requirements.txt','r') as f:
+        INSTALL_REQUIRES = f.read().splitlines()
+
+# get __version__ from _version.py
+ver_file = os.path.join('pymcr', '_version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+VERSION = __version__  # noqa: F821
+
 setup(name='pyMCR',
-      version = '0.3.2',
+      version = VERSION,
       description = 'Multivariate Curve Resolution in Python',
       long_description = long_description,
       url = 'https://github.com/usnistgov/pyMCR',
@@ -18,7 +29,7 @@ setup(name='pyMCR',
       packages = find_packages(),
       zip_safe = False,
       include_package_data = True,
-      install_requires=['numpy', 'scipy'],
+      install_requires=INSTALL_REQUIRES,
       classifiers=['Development Status :: 3 - Alpha',
                    'Intended Audience :: Developers',
                    'Intended Audience :: Science/Research',
@@ -28,6 +39,8 @@ setup(name='pyMCR',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
                    'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
                    'Topic :: Scientific/Engineering :: Information Analysis',
                    'Topic :: Scientific/Engineering :: Bio-Informatics',
                    'Topic :: Scientific/Engineering :: Chemistry',
